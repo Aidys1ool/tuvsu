f41430c087fecb7e4ab8557bc7325e3cefb915df
Aidys1ool <boss.aydys@inbox.ru>
2016-11-21 13:06:05 +0300

    View страницы авторизации

--- tuvsu/Resources/Resource.Designer.cs
+++ tuvsu/Resources/Resource.Designer.cs
@@ -41,11 +41,69 @@ namespace tuvsu
 			}
 		}
 		
+		public partial class Color
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int auth_btn_backg = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int auth_error = 2131034114;
+			
+			// aapt resource value: 0x7f050000
+			public const int auth_linerLayoutBackg = 2131034112;
+			
+			// aapt resource value: 0x7f050004
+			public const int btn = 2131034116;
+			
+			// aapt resource value: 0x7f050005
+			public const int text = 2131034117;
+			
+			// aapt resource value: 0x7f050003
+			public const int titleColor = 2131034115;
+			
+			static Color()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Color()
+			{
+			}
+		}
+		
 		public partial class Drawable
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int ic_launcher = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int imageSlider1 = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int imageSlider2 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int imageSlider3 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int img2 = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int logo_tuvgu = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int news_icon = 2130837511;
+			
+			// aapt resource value: 0x7f020008
+			public const int not_avatar = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int progressBarStyle = 2130837513;
 			
 			static Drawable()
 			{
@@ -60,8 +118,41 @@ namespace tuvsu
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			// aapt resource value: 0x7f060009
+			public const int absoluteLayout1 = 2131099657;
+			
+			// aapt resource value: 0x7f060008
+			public const int authGuest = 2131099656;
+			
+			// aapt resource value: 0x7f060007
+			public const int authStud = 2131099655;
+			
+			// aapt resource value: 0x7f060006
+			public const int checkBoxPass = 2131099654;
+			
+			// aapt resource value: 0x7f06000b
+			public const int errorText = 2131099659;
+			
+			// aapt resource value: 0x7f060003
+			public const int imageView1 = 2131099651;
+			
+			// aapt resource value: 0x7f060001
+			public const int linearLayout1 = 2131099649;
+			
+			// aapt resource value: 0x7f060004
+			public const int login = 2131099652;
+			
+			// aapt resource value: 0x7f060005
+			public const int password = 2131099653;
+			
+			// aapt resource value: 0x7f06000a
+			public const int progressBar1 = 2131099658;
+			
+			// aapt resource value: 0x7f060002
+			public const int relativeLayout1 = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int scrollView1 = 2131099648;
 			
 			static Id()
 			{
@@ -77,7 +168,10 @@ namespace tuvsu
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Auth = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
@@ -92,11 +186,17 @@ namespace tuvsu
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int ApplicationName = 2130968577;
-			
 			// aapt resource value: 0x7f040000
-			public const int Hello = 2130968576;
+			public const int ApplicationName = 2130968576;
+			
+			// aapt resource value: 0x7f040003
+			public const int auth_error = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int auth_guest = 2130968578;
+			
+			// aapt resource value: 0x7f040001
+			public const int auth_open = 2130968577;
 			
 			static String()
 			{
